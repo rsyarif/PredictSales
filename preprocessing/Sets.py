5fdf37cb79c941525cc3a7ea6265ee2abedad3a6
Rizki Syarif <rizki_syarif@brown.edu>
2018-10-12 15:54:18 -0400

    Adding Sets Class

---
+++ preprocessing/Sets.py
@@ -0,0 +1,288 @@
+import numpy as np
+import pandas as pd
+import os
+
+class Sets:
+
+	def __init__(self,**kwargs):
+
+		# self.sales_2013 = kwargs['sales_2013']
+		# self.sales_2014 = kwargs['sales_2014']
+		# self.sales_2015 = kwargs['sales_2015']
+
+		path = '/Users/rizki/Dropbox/Coursera/AML_HowToKaggle/FinalProject/PredictSales/all/'
+
+		self.items = pd.read_csv(path+'items.csv')
+		item_categories = pd.read_csv(path+'item_categories.csv')
+		shops = pd.read_csv(path+'shops.csv')
+		self.sales_train = pd.read_csv(path+'sales_train.csv')
+		self.test = pd.read_csv(path+'test.csv')
+
+		# Format 'date' to datetime
+		self.sales_train['date'] = pd.to_datetime(self.sales_train['date'],format='%d.%m.%Y')
+
+		# Add item_category_id to sales_train
+		self.sales_train = pd.merge(self.sales_train,self.items[['item_id','item_category_id']],on='item_id',how='left').sort_values(by='item_id')
+
+		#add new columns: years, month, Y_M
+		self.sales_train['year'] = self.sales_train['date'].dt.year
+		self.sales_train['month'] = self.sales_train['date'].dt.month
+		self.sales_train_year = self.sales_train['date'].dt.year.astype('string')
+		self.sales_train_month = self.sales_train['date'].dt.month.astype('string')
+		self.sales_train['Y_M'] = self.sales_train_year+'_'+self.sales_train_month
+
+		self.data = {
+				'items':self.items,
+				'item_categories':item_categories,
+				'shops':shops,
+				'sales_train':self.sales_train,
+				'test':self.test,
+				}
+
+		#options
+		self.lag_length = kwargs['lag_length']
+		self.diff = kwargs['diff']
+		self.diffRel = kwargs['diffRel']
+		self.item_cat_count_feat = kwargs['item_cat_count_feat']
+		self.target = kwargs['target']
+
+		print 'lag_length:',self.lag_length
+		print 'diff:',self.diff
+		print 'diffRel:',self.diffRel
+		print 'item_cat_count_feat :',self.item_cat_count_feat
+		print 'target:',self.target
+
+		self.col_to_keep = kwargs['col_to_keep']
+		self.groupby_list= kwargs['groupby_list']
+		self.agg_dict= kwargs['agg_dict']
+		self.agg_targ= kwargs['agg_targ']
+		self.col_targets= kwargs['col_targets']
+
+
+	def splitDataByYear(self):
+		#split by year
+
+		print '\nSplitting sales_train to sales_train_2013, sales_train_2014, and sales_train_2015.'
+
+		self.sales_2013 = self.sales_train[self.sales_train['year']==2013]
+		self.sales_2014 = self.sales_train[self.sales_train['year']==2014]
+		self.sales_2015 = self.sales_train[self.sales_train['year']==2015]
+
+		data = {
+				'sales_2013':self.sales_2013,
+				'sales_2014':self.sales_2014,
+				'sales_2015':self.sales_2015
+				}
+
+		self.data.update(data)
+
+	def getData(self):
+		#updates to latest data
+		print '\nRetrieving latest (preprocessed) data'
+		return self.data
+
+	def checkDuplicates(self):
+
+		obj = ['sales_train','test']
+
+		dup_ids = []
+
+		for i,name in enumerate(obj):
+			print '\nChecking for duplicates in',name
+			dup = self.data[name][self.data[name].duplicated()].index.values
+			dup_ids.append(dup) 
+			if len(dup)>0:
+				print '\nFound {} duplicates in {} : {}'.format(len(dup),name,dup)
+				self.data[name].drop_duplicates(keep='first',inplace=True)
+				print 'Kept first, removed duplicates'
+			else:
+				print 'Found no duplicates in {}'.format(name)
+
+		return dup_ids
+
+	# def checkOutliers(self):
+	# def clipSalesCount(self):
+
+	def createTrainSet(self):
+
+		x_train = self.sales_2013[self.sales_2013['month']==11]
+		x_train = x_train[self.col_to_keep]
+
+		#target features:
+		#agg shop_item 
+		x_train_shop_item = x_train.groupby(self.groupby_list,as_index=False).agg(self.agg_dict).rename(columns={'item_cnt_day':'shop_item_cnt_month'})
+		#agg shop 
+		x_train_shop = x_train[['shop_id','item_cnt_day']].groupby(['shop_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'shop_cnt_month'})
+		#agg item 
+		x_train_item = x_train[['item_id','item_cnt_day']].groupby(['item_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'item_cnt_month'})
+		#agg item_cat 
+		if(self.item_cat_count_feat):x_train_itemcat = x_train[['item_category_id','item_cnt_day']].groupby(['item_category_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'item_cat_cnt_month'})
+
+		#add new columns: shop_item_id
+		x_train_shop_item['shop_item_id']=x_train_shop_item['shop_id'].astype('string')+'_'+x_train_shop_item['item_id'].astype('string')
+
+		#merge
+		x_train_shop_item = pd.merge(x_train_shop_item,x_train_shop,on=['shop_id'],how='left')
+		x_train_shop_item = pd.merge(x_train_shop_item,x_train_item,on=['item_id'],how='left')
+		if(self.item_cat_count_feat):x_train_shop_item = pd.merge(x_train_shop_item,x_train_itemcat,on=['item_category_id'],how='left')
+
+
+		#introduce lag features 10 months behind.
+		for i in xrange(1,self.lag_length+1):
+		    x_train_lag = self.sales_2013[self.sales_2013['month']==(11-i)]
+		    x_train_lag = x_train_lag[self.col_to_keep]
+
+		    #agg shop_item 
+		    x_train_shop_item_lag = x_train_lag.groupby(self.groupby_list,as_index=False).agg(self.agg_dict).rename(columns={'item_cnt_day':'shop_item_cnt_month_lag_'+str(i)})
+		    x_train_shop_item_lag.drop(columns=['item_category_id'],inplace=True)
+		    #agg shop 
+		    x_train_shop_lag = x_train_lag[['shop_id','item_cnt_day']].groupby(['shop_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'shop_cnt_month_lag_'+str(i)})
+		    #agg item 
+		    x_train_item_lag = x_train_lag[['item_id','item_cnt_day']].groupby(['item_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'item_cnt_month_lag_'+str(i)})
+		    #agg item_cat 
+		    if(self.item_cat_count_feat):x_train_itemcat_lag = x_train_lag[['item_category_id','item_cnt_day']].groupby(['item_category_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'item_cat_cnt_month_lag_'+str(i)})
+
+		    #merge
+		    x_train_shop_item = pd.merge(x_train_shop_item,x_train_shop_item_lag,on=['shop_id','item_id'],how='left')
+		    x_train_shop_item = pd.merge(x_train_shop_item,x_train_shop_lag,on=['shop_id'],how='left')
+		    x_train_shop_item = pd.merge(x_train_shop_item,x_train_item_lag,on=['item_id'],how='left')
+		    if(self.item_cat_count_feat):x_train_shop_item = pd.merge(x_train_shop_item,x_train_itemcat_lag,on=['item_category_id'],how='left')
+		    
+		    #self.diffs
+		    for col in ['shop_item','shop','item','item_cat']:
+		        if(not self.item_cat_count_feat and col=='item_cat'): continue #skip item_cat if this feat is not turned on 
+
+		        if i==1:
+		            if(self.diff):x_train_shop_item[col+'_cnt_month_diff({}-{})'.format(str(i-1),str(i))] = x_train_shop_item[col+'_cnt_month'] - x_train_shop_item[col+'_cnt_month_lag_'+str(i)]   
+		        if i>=2:
+		            if(self.diff):x_train_shop_item[col+'_cnt_month_diff({}-{})'.format(str(i-1),str(i))] = x_train_shop_item[col+'_cnt_month_lag_'+str(i-1)] - x_train_shop_item[col+'_cnt_month_lag_'+str(i)]   
+		        
+		        if(self.diffRel):x_train_shop_item[col+'_cnt_month_({}-{})/{}'.format(str(i-1),str(i),str(i))] = x_train_shop_item[col+'_cnt_month_diff({}-{})'.format(str(i-1),str(i))] / (x_train_shop_item[col+'_cnt_month_lag_'+str(i)]+1e-7)   
+
+		x_train_shop_item.fillna(0,inplace=True)
+
+		#pick (meta)self.target            
+		y_train = x_train_shop_item[self.target]
+
+		#remove self.targets(s):
+		x_train = x_train_shop_item.drop(columns=self.col_targets)
+
+		print 'x_train.shape :',x_train.shape
+		print 'y_train.shape :',y_train.shape
+
+		return x_train, y_train
+
+	def createValSet(self):
+
+		x_val = self.sales_2014[self.sales_2014['month']==11]
+		x_val = x_val[self.col_to_keep]
+
+		#agg shop_item 
+		x_val_shop_item = x_val.groupby(self.groupby_list,as_index=False).agg(self.agg_dict).rename(columns={'item_cnt_day':'shop_item_cnt_month'})
+		#agg shop 
+		x_val_shop = x_val[['shop_id','item_cnt_day']].groupby(['shop_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'shop_cnt_month'})
+		#agg item 
+		x_val_item = x_val[['item_id','item_cnt_day']].groupby(['item_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'item_cnt_month'})
+		#agg item_cat 
+		if(self.item_cat_count_feat):x_val_itemcat = x_val[['item_category_id','item_cnt_day']].groupby(['item_category_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'item_cat_cnt_month'})
+
+		#add new columns: shop_item_id
+		x_val_shop_item['shop_item_id']=x_val_shop_item['shop_id'].astype('string')+'_'+x_val_shop_item['item_id'].astype('string')
+
+		#merge
+		x_val_shop_item = pd.merge(x_val_shop_item,x_val_shop,on=['shop_id'],how='left')
+		x_val_shop_item = pd.merge(x_val_shop_item,x_val_item,on=['item_id'],how='left')
+		if(self.item_cat_count_feat):x_val_shop_item = pd.merge(x_val_shop_item,x_val_itemcat,on=['item_category_id'],how='left')
+
+		#introduce lag features
+		for i in xrange(1,self.lag_length+1):
+		    x_val_lag = self.sales_2014[self.sales_2014['month']==(11-i)]
+		    x_val_lag = x_val_lag[self.col_to_keep]
+
+		    #agg shop_item 
+		    x_val_shop_item_lag = x_val_lag.groupby(self.groupby_list,as_index=False).agg(self.agg_dict).rename(columns={'item_cnt_day':'shop_item_cnt_month_lag_'+str(i)})
+		    x_val_shop_item_lag.drop(columns=['item_category_id'],inplace=True)
+		    #agg shop 
+		    x_val_shop_lag = x_val_lag[['shop_id','item_cnt_day']].groupby(['shop_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'shop_cnt_month_lag_'+str(i)})
+		    #agg item 
+		    x_val_item_lag = x_val_lag[['item_id','item_cnt_day']].groupby(['item_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'item_cnt_month_lag_'+str(i)})
+		    #agg item_cat 
+		    if(self.item_cat_count_feat):x_val_itemcat_lag = x_val_lag[['item_category_id','item_cnt_day']].groupby(['item_category_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'item_cat_cnt_month_lag_'+str(i)})
+
+		    #merge
+		    x_val_shop_item = pd.merge(x_val_shop_item,x_val_shop_item_lag,on=['shop_id','item_id'],how='left')
+		    x_val_shop_item = pd.merge(x_val_shop_item,x_val_shop_lag,on=['shop_id'],how='left')
+		    x_val_shop_item = pd.merge(x_val_shop_item,x_val_item_lag,on=['item_id'],how='left')
+		    if(self.item_cat_count_feat):x_val_shop_item = pd.merge(x_val_shop_item,x_val_itemcat_lag,on=['item_category_id'],how='left')
+		    
+		    #self.diffs
+		    for col in ['shop_item','shop','item','item_cat']:
+		        if(not self.item_cat_count_feat and col=='item_cat'): continue #skip item_cat if this feat is not turned on 
+
+		        if i==1: 
+		            if(self.diff):x_val_shop_item[col+'_cnt_month_diff({}-{})'.format(str(i-1),str(i))] = x_val_shop_item[col+'_cnt_month'] - x_val_shop_item[col+'_cnt_month_lag_'+str(i)]   
+		        if i>=2: 
+		            if(self.diff):x_val_shop_item[col+'_cnt_month_diff({}-{})'.format(str(i-1),str(i))] = x_val_shop_item[col+'_cnt_month_lag_'+str(i-1)] - x_val_shop_item[col+'_cnt_month_lag_'+str(i)]   
+
+		        if(self.diffRel):x_val_shop_item[col+'_cnt_month_({}-{})/{}'.format(str(i-1),str(i),str(i))] = x_val_shop_item[col+'_cnt_month_diff({}-{})'.format(str(i-1),str(i))] / (x_val_shop_item[col+'_cnt_month_lag_'+str(i)]+1e-7)   
+
+		x_val_shop_item.fillna(0,inplace=True)
+
+		#pick (meta)self.target            
+		y_val = x_val_shop_item[self.target]
+
+		#remove self.targets(s):
+		x_val = x_val_shop_item.drop(columns=self.col_targets)
+
+		print 'x_val.shape :',x_val.shape
+		print 'y_val.shape :',y_val.shape
+
+		return x_val,y_val
+
+	def createTestSet(self):
+
+		x_test = self.test.sort_values(by=self.groupby_list)
+
+		#add item_category_id.
+		x_test = pd.merge(x_test,self.items[['item_id','item_category_id']],on='item_id',how='left')
+
+		#add new columns: shop_item_id
+		x_test['shop_item_id']=x_test['shop_id'].astype('string')+'_'+x_test['item_id'].astype('string')
+
+		#introduce lag features
+		for i in xrange(1,self.lag_length+1):
+		    x_test_lag = self.sales_2015[self.sales_2015['month']==(11-i)]
+		    x_test_lag = x_test_lag[self.col_to_keep]
+
+		    #agg shop_item 
+		    x_test_shop_item_lag = x_test_lag.groupby(self.groupby_list,as_index=False).agg(self.agg_dict).rename(columns={'item_cnt_day':'shop_item_cnt_month_lag_'+str(i)})
+		    x_test_shop_item_lag.drop(columns=['item_category_id'],inplace=True)
+		    #agg shop 
+		    x_test_shop_lag = x_test_lag[['shop_id','item_cnt_day']].groupby(['shop_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'shop_cnt_month_lag_'+str(i)})
+		    #agg item 
+		    x_test_item_lag = x_test_lag[['item_id','item_cnt_day']].groupby(['item_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'item_cnt_month_lag_'+str(i)})
+		    #agg item_cat 
+		    if(self.item_cat_count_feat):x_test_itemcat_lag = x_test_lag[['item_category_id','item_cnt_day']].groupby(['item_category_id'],as_index=False).agg(self.agg_targ).rename(columns={'item_cnt_day':'item_cat_cnt_month_lag_'+str(i)})
+
+		    #merge
+		    x_test = pd.merge(x_test,x_test_shop_item_lag,on=['shop_id','item_id'],how='left')
+		    x_test = pd.merge(x_test,x_test_shop_lag,on=['shop_id'],how='left')
+		    x_test = pd.merge(x_test,x_test_item_lag,on=['item_id'],how='left')
+		    if(self.item_cat_count_feat):x_test = pd.merge(x_test,x_test_itemcat_lag,on=['item_category_id'],how='left')
+		    
+		    #self.diffs
+		    for col in ['shop_item','shop','item','item_cat']:
+		        if(not self.item_cat_count_feat and col=='item_cat'): continue #skip item_cat if this feat is not turned on 
+		        if i>=2:  
+		            if(self.diff):x_test[col+'_cnt_month_diff({}-{})'.format(str(i-1),str(i))] = x_test[col+'_cnt_month_lag_'+str(i-1)] - x_test[col+'_cnt_month_lag_'+str(i)]   
+		            if(self.diffRel):x_test[col+'_cnt_month_({}-{})/{}'.format(str(i-1),str(i),str(i))] = x_test[col+'_cnt_month_diff({}-{})'.format(str(i-1),str(i))] / (x_test[col+'_cnt_month_lag_'+str(i)]+1e-7)   
+
+
+		x_test = x_test.fillna(0)
+		x_test.drop(columns=['ID'],inplace=True)
+
+		print 'x_test.shape :',x_test.shape
+		#x_test.head()
+
+		return x_test
+
